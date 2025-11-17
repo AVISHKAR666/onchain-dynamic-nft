8aecae431a57a0ddc1110602c4decf530f32f94b
maverick <avichavannda@gmail.com>
2025-11-17 08:14:13 +0000

    style: fix formatting issues

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -4,25 +4,21 @@ pragma solidity ^0.8.28;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-
-contract MoodNft is ERC721{
-
+contract MoodNft is ERC721 {
     uint256 public s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgIamgeUri;
-    mapping (uint256 => Mood) public s_tokenIdToMood;
+    mapping(uint256 => Mood) public s_tokenIdToMood;
 
     enum Mood {
         HAPPY,
         SAD
     }
 
-    constructor(
-        string memory sadSvgImageUri, 
-        string memory happySvgIamgeUri) ERC721("Mood NFT", "MN") {
-            s_tokenCounter = 0;
-            s_sadSvgImageUri = sadSvgImageUri;
-            s_happySvgIamgeUri = happySvgIamgeUri;
+    constructor(string memory sadSvgImageUri, string memory happySvgIamgeUri) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgIamgeUri = happySvgIamgeUri;
     }
 
     function mintNft() public {
@@ -31,31 +27,33 @@ contract MoodNft is ERC721{
         s_tokenCounter++;
     }
 
-    function _baseURI() internal pure override returns(string memory) {
-        return"data:application/json;base64,";
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
     }
 
-    function tokenURI(uint256 tokenId) public view override returns(string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
         string memory imageURI;
-        if(s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
             imageURI = s_happySvgIamgeUri;
         } else {
             imageURI = s_sadSvgImageUri;
         }
-        
-        return 
-        string(
+
+        return string(
             abi.encodePacked(
                 _baseURI(),
                 Base64.encode(
                     abi.encodePacked(
-                        '{"name": "', name(),
+                        '{"name": "',
+                        name(),
                         '", "description": "An NFT that reflects the owners mood.",',
-                        '"attributes": [{"trait_type": "moodiness", "value": 100}],', 
-                        '"image": "', imageURI,'"}'
+                        '"attributes": [{"trait_type": "moodiness", "value": 100}],',
+                        '"image": "',
+                        imageURI,
+                        '"}'
                     )
                 )
             )
-        ); 
+        );
     }
 }
